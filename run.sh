ac994ccca539836055ce346d91e8c45235137a88
Wooter <rick@astley.nevergonnagiveyouup>
2022-04-07 10:03:44 +0200

    Improve run script

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd workdir
 echo "Downloading the tiny core linux kernel"
 wget -nc -O vmlinuz http://tinycorelinux.net/13.x/x86_64/release/distribution_files/vmlinuz64
 echo "Now starting!"
-qemu-system-x86_64 -kernel vmlinuz -initrd out.cpio.gz
+qemu-system-x86_64 -kernel vmlinuz -initrd out.cpio.gz -vga none -append "console=ttyS0 loglevel=3" -monitor none -parallel none -display sdl || qemu-system-x86_64 -kernel vmlinuz -initrd out.cpio.gz -vga none -append "console=ttyS0 loglevel=3" -monitor none -parallel none 
 exit 0
