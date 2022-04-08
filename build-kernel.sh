35daacad85ce1d851aa2950fa5aaae9eb4d7645b
wooter6928196219911 <89206182+wooter6928196219911@users.noreply.github.com>
2022-04-08 06:00:24 +0200

    Extract the kernel while downloading it

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo "Preparing to build the kernel"
-sudo apt update || echo Cannot install elf
-sudo apt install -y libelf-dev || true
+sudo apt update || echo Not a debian distro falling back to pacman; sudo pacman -Sy base-devel libelf || echo Will not install needed deps
+sudo apt install -y libelf-dev bison bc flex gcc || true
 
 mkdir -v workdir || echo "Was made"
 cd workdir
-wget -O linux.tar.gz https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.32.tar.xz
-tar xvf linux.tar.gz
+wget -O - https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.32.tar.xz | xz -d | tar x
 mv linux-5.15.32 linux
 cp ../kconfig linux/.config
 cd linux
